3d4a7672c1dae3152ef7686a90305b5ab4ae215a
rajuahmed118124 <rajuahmed118124@gmail.com>
2023-10-16 11:44:38 +0600

    Initial Commit

---
+++ A - Bitwise Exclusive Or.cpp
@@ -0,0 +1,35 @@
+                                                            ///BISMILLAHIR RAHMANIR RAHIM 
+                                                           ///============================
+/*
+Problem name : A - Bitwise Exclusive Or
+Problem link : https://atcoder.jp/contests/abc213/tasks/abc213_a?lang=en
+Platform     : AtCoder
+Author       : Md. Raju Miah
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define Raju ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+
+void solve()
+{
+    ll a,b;
+    cin>>a>>b;
+    cout<<(a^b)<<endl;
+}
+
+
+int main()
+{
+    Raju;
+    solve();
+return 0;
+}
+
+
+                                                                     ///ALHAMDULILLAH
